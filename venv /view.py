2a68c873d397ed6c7743ed996d8ba089b0f1c960
CeliaBkln <bouaklinecelia@gmail.com>
2024-03-21 18:43:24 +0100

    - modif view

--- venv /view.py
+++ venv /view.py
@@ -14,11 +14,3 @@ def plot_portfolio_returns(portfolio_returns):
 def plot_volatility(volatility_series):
     volatility_series.plot().set_ylabel("Annualized Volatility, 30-day Window")
     plt.show()
-
-
-def display_covariance_matrix(covariance_matrix):
-    print(f"Covariance Matrix:\n{covariance_matrix}")
-
-
-def display_portfolio_volatility(portfolio_volatility):
-    print(f"Portfolio Volatility: {portfolio_volatility:.2f}%")
